e8b2aa267c84d7a170755e8e706fdcc5752cc384
용진 <k45824@kaist.ac.kr>
2023-09-19 12:31:06 +0900

    study_230919_start

--- PracticeAssemble/Day2_5_casting.c
+++ PracticeAssemble/Day2_5_casting.c
@@ -10,7 +10,26 @@ int main(void)
 
     // 소수부 손실, 상위 바이트 잘라내기. -> csapp
 
-    
+    // CPU가 처리하기에 가장 적합한 크기의 정수 자료형이 int. 그러니 자동 형 변환이 되어 연산이 진행된다.
+    short n1 = 3, n2 = 4;
+    short n3 = n1+n2; // 정수의 승격 (int로 형변환되어 계산하고 그 값을 n3에 저장하니)
+    //근데 이로 인해 int에서 short로 줄어드는 형 변환이 다시 일어남.
+
+    // int와 double의 연산에서는 double로 형 변환되어 계산
+    // 피연산자의 자료형이 일치하지 않아서 발생하는 자동 형 변환은 데이터의 손실을 최소화하는 방향으로 진행
+    // int -> long -> long long -> float -> double -> long double
+
+    double d1, d2;
+    d1 = n1/n2;
+    printf("%f \n", d1);
+
+    // 위의 실행 결과는 0.0000이다. 왜?
+    // 당연하다. int의 연산을 처리한 값이 double d에 저장되니 int 계산상에서 이미 사라진 소수자리를 받아갈 수 없었던 것.
+
+    d2 = (double)n1/n2;
+    printf("%f \n", d2);
+    // 이렇게 하면 원하던 결과가 나온다.
 
 
+    return 0;
 }
--- Test.c
+++ Test.c
@@ -9,5 +9,5 @@ int main()
 /*
 항상 새로운 c확장자 파일을 만들면 ctrl+s 저장.
 ctrl+alt+c (혹은 ctrl+shift+b)를 눌러서 save and compile for c를 클릭.
-저걸 하고 나서야 ctrl+alt+r 해서 실행할 수 있음.
+저걸 하고 나서야 ctrl+alt+v 해서 실행할 수 있음. -> ㅏkeybindings.json 체크
 */
