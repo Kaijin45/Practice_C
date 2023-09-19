3afde65f4ae69c3b198b35d0aca4255fc764f8cc
용진 <k45824@kaist.ac.kr>
2023-09-19 12:37:52 +0900

    Orgainize_230919_ToSeeWell

---
+++ DAY3/Day3_1_printfANDscanf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // printf는 문자열을 출력하는 함수
+    // printf에서 ""는 문자열의 시작과 끝으로 인식, 큰 따옴표 자체의 출력을 바란다면 \를 붙여주자.
+    printf("Hello world? \n \"Hi\" \n My world says. \n ");
+    
+
+    return 0;
+}
